5e6ea749c0034a7b06f10373a4b3d6fabce68a6e
Juan Loaiza <juanmazo1234@gmail.com>
2024-09-16 23:43:12 -0500

    se agregan: Logo, home page, links en el footer, paginas de about, privacy policies, login, sigin y se linkean a los botones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signin', component: SigninComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+];

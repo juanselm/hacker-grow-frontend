01ca46dcbcd39e947b20641d4e3b24504cc8fb08
Juan Loaiza <juanmazo1234@gmail.com>
2024-10-01 18:57:49 -0500

    se crea el componente de registro

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })

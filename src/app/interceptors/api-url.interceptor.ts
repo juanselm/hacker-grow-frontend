b6f1cc083b701c0b7e427c78eee49945632ebf8d
Juan Loaiza <juanmazo1234@gmail.com>
2024-10-07 22:26:02 -0500

    Se agerga el url api a los enviroments

---
+++ src/app/interceptors/api-url.interceptor.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './../../enviroments/environment';
+
+@Injectable()
+export class ApiUrlInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const apiReq = req.clone({ url: `${environment.apiUrl}${req.url}` });
+    return next.handle(apiReq);
+  }
+}

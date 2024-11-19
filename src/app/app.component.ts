70c02a2e4eeb246892bcac0f0531b5d0de77d28d
Juan Loaiza <juanmazo1234@gmail.com>
2024-11-19 16:13:09 -0500

    se agrega cambio en los botones cuando el user este logueado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './services/auth.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'hacker-grow-frontend';
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/home']);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { environment } from './../../environments/environment';
 
 @Injectable({
@@ -8,6 +8,7 @@ import { environment } from './../../environments/environment';
 })
 export class AuthService {
   private apiUrl = `${environment.apiUrl}/api/users`;
+  private loggedIn = new BehaviorSubject<boolean>(this.isLoggedIn());
 
   constructor(private http: HttpClient) {}
 
@@ -25,7 +26,16 @@ export class AuthService {
     return !!localStorage.getItem('user');
   }
 
+  get isLoggedIn$(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+
   logout(): void {
     localStorage.removeItem('user');
+    this.loggedIn.next(false);
+  }
+
+  setLoggedIn(value: boolean): void {
+    this.loggedIn.next(value);
   }
 }
